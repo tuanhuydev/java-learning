f7ab7cd90267592503f5c8c3d680fcea059c505b
tuanhuydev <tuanhuydev@gmail.com>
2024-02-17 11:54:23 +0700

    feat(package): learning package

---
+++ learning/MyArrayList.java
@@ -0,0 +1,11 @@
+package learning;
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Honda");
+        cars.add("Toyota");
+        System.out.println(cars);
+    }
+}
